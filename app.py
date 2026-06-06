93c169d8be3169aa782ff838be04e0f7035dcd61
Biloliddin <tursinalinurmatov8@email.com>
2026-06-06 11:23:10 +0500

    Ilk commit - bot fayllari qo'shildi

---
+++ app.py
@@ -0,0 +1,26 @@
+import asyncio
+import logging
+
+from loader import bot, dp
+import middlewares, filters, handlers
+from utils.notify_admins import on_startup_notify
+from utils.set_bot_commands import set_default_commands
+
+
+async def on_startup():
+    await set_default_commands(bot)
+    await on_startup_notify(bot)
+
+
+async def main():
+    logging.basicConfig(level=logging.INFO)
+    middlewares.setup(dp)
+    filters.setup(dp)
+    handlers.setup(dp)
+
+    await on_startup()
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
---
+++ handlers/users/__init__.py
@@ -0,0 +1,10 @@
+from aiogram import Router
+
+from .help import router as help_router
+from .start import router as start_router
+from .echo import router as echo_router
+
+router = Router()
+router.include_router(start_router)
+router.include_router(help_router)
+router.include_router(echo_router)
